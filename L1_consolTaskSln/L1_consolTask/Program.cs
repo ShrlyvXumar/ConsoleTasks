f44bf1e9ee5da35584b5c973309bf75c89dd6960
ShrlyvXumar <khumar2001@gmail.com>
2024-07-31 12:44:32 +0400

    Console Tasks

---
+++ L1_consolTaskSln/L1_consolTask/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+
+namespace L1_consolTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Company About
+            string company = "Samsung";
+            Console.WriteLine("Information about the company \nOur mission and values \n" + company + " executives and employees are convinced that adhering to the right values is the key to business success. Therefore, Samsung is guided by core corporate values ​​and a strict code of business ethics in every decision it makes. \n");
+            //Company official partners
+            string officialPartnersKontackt = "Kontackt";
+            string officialPartnersBakuElectronics = "Baku Electronics";
+            string officialPartnersIrshad = "Irşad";
+            string officialPartnersSoliton = "Soliton";
+            string officialPartnersWorldTelecom = "World Telecom";
+            Console.WriteLine("Meet our official partners: " + officialPartnersKontackt + ", " + officialPartnersBakuElectronics + ", " + officialPartnersIrshad + ", " + officialPartnersSoliton + ", " + officialPartnersWorldTelecom);
+            //Baku Electronics Location and Phone Number
+            string bakuElectronicsLocation = "Baku";
+            string bakuElectronicsPhoneNumber = "143";
+            string test = officialPartnersBakuElectronics + "\tLoaction: " + bakuElectronicsLocation + "\tPhone Number: " + bakuElectronicsPhoneNumber;
+            Console.WriteLine(test);
+            //Console.WriteLine("\n-" + officialPartnersBakuElectronics + "\tLoaction: " + bakuElectronicsLocation + "Phone Number: " + bakuElectronicsPhoneNumber);
+            //Smart Phones Brands
+            string smartPhonesBrandsApple = "Apple";
+            string smartPhonesBrandsSamsung = "Samsung";
+            string smartPhonesBrandsXiaomi = "Xiaomi";
+            Console.WriteLine("\nBaku Electronics -> Smart Phones Brands: " + smartPhonesBrandsApple + ", " + smartPhonesBrandsSamsung + ", " + smartPhonesBrandsXiaomi);
+            //Smart Phone
+            string smartPhoneBrand = "Apple";
+            string smartPhoneModel = "IPHONE 15 PRO MAX 256GB BLUE TITANIUM";
+            string smartPhoneColor = "Black";
+            ushort smartPhoneMemoryGb = 512;
+            ushort smartPhonePrice = 3400;
+            bool isBuyCredit = true;
+            byte time = 1;
+            Console.WriteLine("Smart Phone Brand: " + smartPhoneBrand + "\nSmart Phone Model: " + smartPhoneModel + "\nSmart Phone Color: " + smartPhoneColor + "\nSmart Phone Memory Gb: " + smartPhoneMemoryGb + "\nSmart Phone Price: " + smartPhonePrice + "\nCredit: " + isBuyCredit + "\nTime: " + time);
+            //Computer Brands
+            string computerBrandsMSI = "MSI";
+            string computerBrandsLenova = "Lenova";
+            string computerBrandsHp = "HP";
+            Console.WriteLine("\nBaku Electronics -> Computer Brands: " + computerBrandsMSI + ", " + computerBrandsLenova + ", " + computerBrandsHp);
+            //Computer MSI
+            string computerBrand = "MSI";
+            string computerModel = "Katana 15/ASPECT RATIO IPS 144HZ I7-13620H 16GB 1TB NV R";
+            string computerColor = "Black";
+            string computerCPU = "2.4 GHz - 4.9 GHz";
+            string computerProcessorType = "Core i7";
+            ushort computerRamGb = 16;
+            ushort computerRom = 1;
+            ushort computerVramGb = 8;
+            float computerScreenSize = 15.6F;
+            ushort computerPrice = 3700;
+            bool isOS = false;
+            bool isMicrophone = true;
+            bool isWlan = true;
+            bool isBluetooth = true;
+            Console.WriteLine("\nComputer Brand: " + computerBrand + "\nComputer Model: " + computerModel + "\nComputer Color: " + computerColor + "\nComputer Screen Size (\"): " + computerScreenSize + "\nComputer CPU: " + computerCPU + "\nComputer Processor Type: " + computerProcessorType + "\nComputer RAM (GB): " + computerRamGb + "\nComputer ROM (GB): " + computerRom + "\nComputer Video card memory (GB): " + computerVramGb + "\nComputer Price: " + computerPrice + "\nCredit: " + isBuyCredit + "\nTime: " + time + "\nOS: " + isOS + "\nBuilt-in Microphone: " + isMicrophone + "\nWLAN: " + isWlan + "\nBluetooth: " + isBluetooth);
+        }
+    }
+}
---
+++ L2_Task/L2_TaskSln/L2_Task/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace L2_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 2;
+            int sum = CalculateSum(num1, num2);
+            Console.WriteLine("First number: " + num1 + " | Second number: " + num2);
+            Console.WriteLine("Sum: " + sum);
+            int diff = CalculateDiff(num1, num2);
+            Console.WriteLine("Diff: " + diff);
+            int division = CalculateDivision(num1, num2);
+            Console.WriteLine("Division: " + division);
+            int multiplication = CalculateMultiplication(num1, num2);
+            Console.WriteLine("Multiplication: " + multiplication);
+
+            //DateTime methodlarini yoxlamaq
+            DateTime now = DateTime.Now;
+            Console.WriteLine("\nNow: " + now);
+            int minute = now.Minute;
+            Console.WriteLine("\nMinute: " + minute);
+            int microsecond = now.Microsecond;
+            Console.WriteLine("\nMicrosecond:" + microsecond);
+            int month = now.Month;
+            Console.WriteLine("\nMonth: " + month);
+            int year = now.Year;
+            Console.WriteLine("\nYear: " + year);
+
+            DateTime addDay = now.AddDays(5);
+            Console.WriteLine("\nAdd Day: " + addDay);
+
+            DateTime diffDay = now.AddDays(-5);
+            Console.WriteLine("\nDiff Day: " + diffDay);
+
+            DateTime dateTime = new DateTime(2020, 01, 01, 00, 00, 00);
+            Console.WriteLine("\nOther data time: " + dateTime);
+
+            double diffDays = now.Subtract(dateTime).TotalDays;
+            Console.WriteLine("\nDifference between 2 times (days): " + diffDays);
+
+            double diffSecond = now.Subtract(dateTime).TotalSeconds;
+            Console.WriteLine("\nDifference between 2 times (seconds): " + diffSecond);
+
+            DateTime todaysDate = DateTime.Today;
+            Console.WriteLine("\nToday: " + todaysDate);
+
+            DateTime currentDateTimeUTC = DateTime.UtcNow;
+            Console.WriteLine("\ncurrentDateTimeUTC: " + currentDateTimeUTC);
+
+            DateTime maxDateTimeValue = DateTime.MaxValue;
+            Console.WriteLine("\nmaxDateTimeValue: " + maxDateTimeValue);
+
+            DateTime minDateTimeValue = DateTime.MinValue;
+            Console.WriteLine("\nminDateTimeValue: " + minDateTimeValue);
+        }
+
+        static int CalculateSum(int firstNumber, int secondNumber) 
+        {   
+            int resultSum = 0;
+
+            resultSum =  firstNumber + secondNumber;
+            int result = firstNumber - secondNumber;
+
+            return resultSum;
+        }
+
+        static int CalculateDiff(int firstNumber, int secondNumber)
+        {
+            int resultDiff = 0;
+
+            resultDiff = firstNumber - secondNumber;
+
+            return resultDiff;
+            
+        }
+
+        static int CalculateDivision(int firstNumber, int secondNumber)
+        {
+            int resultDivision = 0;
+
+            resultDivision = firstNumber/secondNumber;
+
+            return resultDivision;
+        }
+
+        static int CalculateMultiplication(int firstNumber, int secondNumber)
+        {
+            int resultMultiplication = 0;
+
+            resultMultiplication = firstNumber * secondNumber;
+
+            return resultMultiplication;
+        }
+    }
+}
